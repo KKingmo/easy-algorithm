bca262281a35e0fafdaf5379ebff1d08e1aa4c6e
Kingmo <55ooccmm@gmail.com>
2022-03-24 11:36:52 +0900

    test: 문자열 뒤집기

---
+++ template/template.test.js
@@ -0,0 +1,17 @@
+/*
+
+*/
+
+function solution(a) {
+  return a;
+}
+
+describe("solution", () => {
+  test("1", () => {
+    expect(solution()).toEqual();
+  });
+
+  test("2", () => {
+    expect(solution()).toEqual();
+  });
+});
